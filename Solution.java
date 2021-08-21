33bfd4ca900377dad7f420020d983a80205ff4b6
Lachezar Kutsarov <lkutsarov@gmail.com>
2021-08-22 00:06:41 +0200

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,20 +1,21 @@
+
 public class Solution {
 
-	public long maxMatrixSum(int[][] matrix) {
-		long sumAbsoluteValues = 0;
-		int minimumAbsoluteValue = Integer.MAX_VALUE;
-		int numOfNegatives = 0;
+    public long maxMatrixSum(int[][] matrix) {
+        long sumAbsoluteValues = 0;
+        int minimumAbsoluteValue = Integer.MAX_VALUE;
+        int numOfNegatives = 0;
 
-		for (int r = 0; r < matrix.length; r++) {
-			for (int c = 0; c < matrix[0].length; c++) {
-				sumAbsoluteValues += Math.abs(matrix[r][c]);
-				minimumAbsoluteValue = Math.min(minimumAbsoluteValue, Math.abs(matrix[r][c]));
-				if (matrix[r][c] < 0) {
-					numOfNegatives++;
-				}
-			}
-		}
-		long result = numOfNegatives % 2 == 0 ? sumAbsoluteValues : sumAbsoluteValues - 2 * minimumAbsoluteValue;
-    return result;
-	}
+        for (int r = 0; r < matrix.length; r++) {
+            for (int c = 0; c < matrix[0].length; c++) {
+                sumAbsoluteValues += Math.abs(matrix[r][c]);
+                minimumAbsoluteValue = Math.min(minimumAbsoluteValue, Math.abs(matrix[r][c]));
+                if (matrix[r][c] < 0) {
+                    numOfNegatives++;
+                }
+            }
+        }
+        long result = numOfNegatives % 2 == 0 ? sumAbsoluteValues : sumAbsoluteValues - 2 * minimumAbsoluteValue;
+        return result;
+    }
 }
